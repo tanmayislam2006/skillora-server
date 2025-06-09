e54260c86f842f45c07c547e9f900c89773e43a7
Your Name <you@example.com>
2025-06-09 17:35:42 +0600

    set up and make the register api

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kn8r7rw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+app.get("/", (req, res) => {
+  res.send("Hello server");
+});
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // await client.connect();
+    const userCollection = client.db("skillora").collection("users");
+ 
+
+    // get a single user
+    app.get("/user/:uid", async (req, res) => {
+      const uid = req.params.uid;
+      const query = { uid: uid };
+      const user = await userCollection.findOne(query);
+      if (!user) {
+        return res.status(404).send({ message: "User not found", user: null });
+      }
+      res.send(user);
+    });
+    // register user
+    app.post("/register", async (req, res) => {
+      const userInformation = req.body;
+      const { email } = userInformation;
+      const query = { email: email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.status(400).send({ message: "User already exists" });
+      }
+      const result = await userCollection.insertOne(userInformation);
+      res.send(result);
+    });
+    // update user login information
+    app.patch("/login", async (req, res) => {
+      const { email, lastSignInTime } = req.body;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {
+          lastSignInTime,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+   
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
